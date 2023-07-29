25a5ff04f609fcbfefd4542041d4a0257a610de3
Seo Wonbin <81563920+Wonbn@users.noreply.github.com>
2023-07-29 17:18:01 +0900

    [Gold V] Title: 괄호 제거, Time: 344 ms, Memory: 23636 KB -BaekjoonHub

--- 백준/Gold/2800. 괄호 제거/괄호 제거.java
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.java
@@ -3,31 +3,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String expression = scanner.nextLine().trim();
-        scanner.close();
+        String expression = scanner.nextLine(); // 입력
 
+        // Set을 활용해서 중복을 제거
         Set<String> result = differentExpressions(expression);
 
+        // 괄호를 무조건 제거해야 하기 때문에 입력한 초기 식은 포함 X
         List<String> list = new ArrayList<>();
         for (String exp : result)
             if(!expression.equals(exp)) list.add(exp);
 
+        // 사전 순 정렬
         Collections.sort(list);
 
         for (String exp : list) System.out.println(exp);
     }
 
-    // 괄호를 제거해서 서로 다른 식을 만들자!
+    // 괄호를 제거해서 찾은 다른 식을 저장하고 반환하는 메서드
     public static Set<String> differentExpressions(String expression) {
         // 중복된 문자열이 저장될 수 있기 때문에 결과는 Set으로 받는다.
         Set<String> result = new HashSet<>();
 
-        // 재귀를 통한 괄호제거를 통한 가능한 모든 경우의 식을 찾는다.
+        // 재귀를 통한 가능한 모든 경우의 식을 찾는다.
         generate(expression, 0, result);
 
         return result;
     }
 
+    // 다른 식을 만드는 메서드
     private static void generate(String expression, int startIndex, Set<String> result) {
         if (startIndex == expression.length()) {
             result.add(expression);
@@ -35,34 +38,31 @@ public class Main {
         }
 
         if (expression.charAt(startIndex) == '(') {
+            // 자신의 쌍인 닫힌 괄호를 찾아서 endIndex에 저장
             int endIndex = findMatchingClosingParenthesis(expression, startIndex);
-            for (int i = startIndex + 1; i < endIndex; i++) {
-                if (expression.charAt(i) == '(') {
-                    generate(expression, i, result);
-                    i = findMatchingClosingParenthesis(expression, i);
-                }
-            }
+
+            // 찾은 괄호 쌍 사이에 있는 괄호를 찾아서 제거하여 다시 식을 만든다.
             generate(expression, startIndex + 1, result);
             generate(expression.substring(0, startIndex) + expression.substring(startIndex + 1, endIndex) +
                     expression.substring(endIndex + 1), startIndex, result);
-        } else {
+        } else
             generate(expression, startIndex + 1, result);
-        }
     }
 
+    //닫힌 괄호를 찾는 메서드
     public static int findMatchingClosingParenthesis(String expression, int startIndex) {
         int count = 0;
         for (int i = startIndex; i < expression.length(); i++) {
             char c = expression.charAt(i);
-            if (c == '(') {
-                count++;
-            } else if (c == ')') {
+            
+            // 괄호가 쌍을 이루었는지에 대한 확인 후,
+            // 자신의 쌍인 닫힌괄호를 만나면 그때의 인덱스를 반환
+            if (c == '(') count++;
+            else if (c == ')') {
                 count--;
-                if (count == 0) {
-                    return i;
-                }
+                if (count == 0) return i;
             }
         }
-        return -1; // Error: No matching closing parenthesis found
+        return -1; // 에러 : 올바른 식이 아닐때
     }
 }
