9ec24835c18e11e4339d2e4d15481a651b6040e4
Seo Wonbin <81563920+Wonbn@users.noreply.github.com>
2023-07-31 11:59:08 +0900

    [Silver III] Title: 1로 만들기, Time: 228 ms, Memory: 21684 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -6,13 +6,17 @@ public class Main {
 
         int n = sc.nextInt();
 
-        int[] count = new int[1000001];
+        int[] count = new int[1000001]; 
+        // 입력값 n에 대한 1~n까지의 최소연산횟수를 저장
         for(int i = 2; i <= n; i++) {
             int min = Integer.MAX_VALUE;
+            // 3으로 나눠지거나 2로 나눠지거나 3,2 둘 모두로 나눠지거나 둘 모두로 나눠지지 않거나
+            // 이렇게 4가지 경우의 수를 모두 커버한다.
             if(i % 3 == 0) min = Math.min(count[i/3], min);
             if(i % 2 == 0) min = Math.min(count[i/2], min);
             min = Math.min(count[i-1], min);
 
+            // 그 중 가장 최소연산을 수행하는 경우로 결과를 저장한다.
             count[i] = min + 1;
         }
 
