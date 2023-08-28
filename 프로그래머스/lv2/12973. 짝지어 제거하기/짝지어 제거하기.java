28912769de131c97f5db2b66f8e7008b4cc27992
Seo Wonbin <81563920+Wonbn@users.noreply.github.com>
2023-08-28 14:33:10 +0900

    [level 2] Title: 짝지어 제거하기, Time: 49.82 ms, Memory: 58.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stk = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(!stk.isEmpty() && stk.peek() == s.charAt(i)) {
+                stk.pop();
+            } else {
+                stk.push(s.charAt(i));
+            }
+        }
+
+        if(stk.isEmpty()) {
+            return 1;
+        }
+        return 0;
+    }
+}
