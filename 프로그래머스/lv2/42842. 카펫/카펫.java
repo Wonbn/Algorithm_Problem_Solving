87095879f8014e0d6057bebf02fc691dbeb14531
Seo Wonbin <81563920+Wonbn@users.noreply.github.com>
2023-09-06 22:33:06 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,17 +1,12 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
+        int area = brown + yellow;
         int[] answer = new int[2];
-        int area = yellow;
-        int a = 0;
-        int b = 0;
-        for(int i = 1; i <= Math.sqrt(area); i++) {
-            if(area % i == 0) {
-                a = i; // 세로
-                b = area/i; // 가로
-            }
-            if(2*(a+b)+4 == brown) {
-                answer[0] = b + 2;
-                answer[1] = a + 2;
+        for(int i = 3; i <= (brown/2+2)/2; i++) {
+            int hl = brown/2+2 - i;
+            if(hl * i == area) {
+                answer[0] = hl;
+                answer[1] = i;
             }
         }
         
