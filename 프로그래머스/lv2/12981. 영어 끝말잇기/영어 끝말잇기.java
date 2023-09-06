490190e390242fda8549f67f5b0f4fd13102f7d0
Seo Wonbin <81563920+Wonbn@users.noreply.github.com>
2023-09-06 22:44:53 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,32 +1,24 @@
 import java.util.*;
 
 class Solution {
-    static Set<String> set = new HashSet<>();
-
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        int[][] now = new int[n][2];
-        char ch = words[0].charAt(words[0].length()-1);
-        int size = 0;
-        set.add(words[0]);
-        now[0][0]++;
-
+        Set<String> alps = new HashSet<>();
+        alps.add(words[0]);
         for(int i = 1; i < words.length; i++) {
-            now[i%n][0]++;
-            set.add(words[i]);
-
-            if(size == set.size() || ch != words[i].charAt(0)) {
+            if(alps.contains(words[i])) {
                 answer[0] = i%n+1;
-                answer[1] = now[i%n][0];
-                return answer;
-            } else {
-                ch = words[i].charAt(words[i].length()-1);
-                size = set.size();
+                answer[1] = i/n+1;
+                break;
             }
+            if(words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
+                answer[0] = i%n+1;
+                answer[1] = i/n+1;
+                break;
+            }
+            alps.add(words[i]);
         }
 
-        answer[0] = 0;
-        answer[1] = 0;
         return answer;
     }
 }
