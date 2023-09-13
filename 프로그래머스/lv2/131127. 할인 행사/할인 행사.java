0525ad84f591d92d3fa65e11a9f0b7fe24411465
Seo Wonbin <81563920+Wonbn@users.noreply.github.com>
2023-09-13 14:46:13 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -3,47 +3,54 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        int n = want.length;
         
-        Map<String, Integer> map = new HashMap<>();
-        
-        int lenW = want.length;
+        // 첫째날
+        Map<String, Integer> discountMap = new HashMap<>();
         for(int i = 0; i < 10; i++) {
-            if(!map.containsKey(discount[i])) {
-                map.put(discount[i], 1);
+            String d = discount[i];
+            if(!discountMap.containsKey(d)) {
+                discountMap.put(d, 1);
                 continue;
-            } 
-            map.put(discount[i], map.get(discount[i])+1);
+            }
+            discountMap.put(d, discountMap.get(d)+1);
         }
         
-        for(int i = 0; i < lenW; i++) {
-            if(!map.containsKey(want[i]) || map.get(want[i]) != number[i]) {
+        for(int i = 0; i < n; i++) {
+            String key = want[i];
+            if(!discountMap.containsKey(key) || discountMap.get(key) != number[i]) {
                 break;
             }
-            if(i == lenW-1) {
+            if(i == n-1) {
                 answer++;
             }
         }
         
-        int lenD = discount.length;
-        for(int i = 0; i < lenD-10; i++) {
-            map.put(discount[i], map.get(discount[i])-1);
-            
-            if(!map.containsKey(discount[i+10])) {
-                map.put(discount[i+10], 1);
-            } else {
-                map.put(discount[i+10], map.get(discount[i+10])+1);
+        // 둘째날 이후
+        int m = discount.length;
+        for(int i = 1; i <= m-10; i++) {
+            discountMap = new HashMap<>();
+            for(int j = i; j < i+10; j++) {
+                String d = discount[j];
+                if(!discountMap.containsKey(d)) {
+                    discountMap.put(d, 1);
+                    continue;
+                }
+                discountMap.put(d, discountMap.get(d)+1);
             }
-            
-            for(int j = 0; j < lenW; j++) {
-                if(!map.containsKey(want[j]) || map.get(want[j]) != number[j]) {
+        
+            for(int j = 0; j < n; j++) {
+                String key = want[j];
+                if(!discountMap.containsKey(key) || discountMap.get(key) != number[j]) {
                     break;
                 }
-                if(j == lenW-1) {
+                if(j == n-1) {
                     answer++;
                 }
             }
         }
         
+    
         return answer;
     }
 }
