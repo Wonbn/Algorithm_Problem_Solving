d133095bcdeaf0f2fc9c1dbf730b159eed302f48
Seo Wonbin <81563920+Wonbn@users.noreply.github.com>
2023-11-07 11:20:07 +0900

    [level 2] Title: 스킬트리, Time: 0.12 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,33 +1,25 @@
-import java.util.*; // 450
+import java.util.*;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        Set<Character> skillSet = new HashSet<>();
-        for(char c : skill.toCharArray()) skillSet.add(c);
+        Set<Character> set = new HashSet<>();
+        char[] cs = skill.toCharArray();
+        for(char c : cs) set.add(c);
         
-        boolean possible = true;
-        int l = skill_trees.length;
-        StringBuilder sb = new StringBuilder(skill);
-        skill = sb.reverse().toString();
-        for(int i = 0; i < l; i++) {
-            Stack<Character> skillTree = new Stack<>();
-            for(char c : skill.toCharArray()) skillTree.push(c);
-            
-            String st = skill_trees[i];
-            for(char c : st.toCharArray()) {
-                if(!skillTree.isEmpty() && skillTree.peek() == c) 
-                    skillTree.pop();
-                else {
-                    if(skillSet.contains(c)) {
-                        possible = false;
+        for(String s : skill_trees) {
+            int idx = 0;
+            boolean check = true;
+            for(char c : s.toCharArray()) {
+                if(set.contains(c)) {
+                    if(c != cs[idx]) {
+                        check = false;
                         break;
-                    }
+                    } else idx++;
                 }
             }
             
-            if(possible) answer++;
-            possible = true;
+            if(check) answer++;
         }
         
         return answer;
