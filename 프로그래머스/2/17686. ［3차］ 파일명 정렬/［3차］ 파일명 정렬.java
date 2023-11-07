2621de204f917c7f445ba2491d6bba0352175ccd
Seo Wonbin <81563920+Wonbn@users.noreply.github.com>
2023-11-07 12:21:17 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 9.80 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,35 +1,50 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String[] files) {
-        Arrays.sort(files, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                String head1 = s1.split("[0-9]")[0];
-                String head2 = s2.split("[0-9]")[0];
-                int compareHead = head1.toUpperCase().compareTo(head2.toUpperCase());
-                if(compareHead == 0) { // head가 같을 때 number 비교
-                    s1 = s1.substring(head1.length());
-                    s2 = s2.substring(head2.length());
-                    String number1 = "";
-                    for(char c : s1.toCharArray()) { // number
-                        if(Character.isDigit(c) && number1.length()<=5)
-                            number1 += c;
-                        else
-                            break;
-                    }
-                    String number2 = "";
-                    for(char c : s2.toCharArray()) {
-                        if(Character.isDigit(c) && number2.length()<=5)
-                            number2 += c;
-                        else
-                            break;
-                    }
-                    return Integer.parseInt(number1) - Integer.parseInt(number2);
-                } else {
-                    return compareHead;
-                }
+        String[] answer = new String[files.length];
+        List<String[]> tmp = new ArrayList<>();
+        
+        for(String file : files) {
+            tmp.add(splitString(file));
+        }
+        
+        tmp.sort((o1, o2) -> {
+            if(o1[0].compareToIgnoreCase(o2[0]) == 0) {
+                return Integer.compare(Integer.parseInt(o1[1]), Integer.parseInt(o2[1]));
+            } else {
+                return o1[0].compareToIgnoreCase(o2[0]);
             }
         });
-        return files;
+        
+        int i = 0;
+        for(String[] tm : tmp) {
+            StringBuilder sb = new StringBuilder();
+            for(String t : tm) sb.append(t);
+            answer[i++] = sb.toString();
+        }
+        
+        return answer;
+    }
+    
+    public String[] splitString(String str) {
+        String[] result = new String[3];
+        int index = 0;
+
+        while(index < str.length() && !Character.isDigit(str.charAt(index))) {
+            index++;
+        }
+        result[0] = str.substring(0, index);
+
+        int start = index;
+        while(index < str.length() && Character.isDigit(str.charAt(index))) {
+            if (index - start == 5) break;
+            index++;
+        }
+        
+        result[1] = str.substring(start, index);
+        result[2] = str.substring(index);
+
+        return result;
     }
 }
